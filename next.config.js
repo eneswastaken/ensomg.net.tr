320b02c6c625dde1d0a3ec585a3c5341dbbaade6
Enes <120328716+eneswastaken@users.noreply.github.com>
2024-05-07 17:54:28 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -10,17 +10,17 @@ module.exports = {
       },
       {
         source: '/twitter',
-        destination: 'https://twitter.com/notcnrad',
+        destination: 'https://twitter.com/ensomgg',
         permanent: true,
       },
       {
         source: '/linkedin',
-        destination: 'https://linkedin.com/in/cnrad',
+        destination: 'https://linkedin.com/',
         permanent: true,
       },
       {
         source: '/new',
-        destination: 'https://h-cnrad.vercel.app/?pinned=https://raw.githubusercontent.com/cnrad/h.cnrad.dev/master/src/bookmarks.json&background=https://source.unsplash.com/random/1920x1080/?sunset,blue,pink',
+        destination: 'https://ensomg.net.tr',
         permanent: true,
       },
     ]
