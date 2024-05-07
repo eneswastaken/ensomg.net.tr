5348fb92bc71dfcba4561d368e2f18020bf1e271
Enes <120328716+eneswastaken@users.noreply.github.com>
2024-05-07 18:08:23 +0300

    Update RepoItem.tsx

--- src/components/RepoItem.tsx
+++ src/components/RepoItem.tsx
@@ -17,7 +17,7 @@ interface RepoProps {
 
 const RepoItem = ({ name, description, stars, forks, language }: RepoProps) => {
     return (
-        <a href={`https://github.com/cnrad/${name}`} rel="noreferrer" target="_blank">
+        <a href={`https://github.com/ensomgg/${name}`} rel="noreferrer" target="_blank">
             <div className="flex flex-col h-36 p-4 bg-white/10 dark:bg-black/10 rounded-md border border-slate-400 hover:border-slate-700 dark:border-slate-800 dark:hover:border-slate-600 transition-colors duration-75 cursor-pointer">
                 <h1 className="font-semibold mb-1">{name}</h1>
                 <p className="text-sm text-gray-800/70 dark:text-gray-100/70">{description}</p>
